1835ca97e0d2d37c20bc81bc750f5edc9b5bc4f8
Aria Stewart <aredridel@nbtsc.org>
2012-10-21 18:00:06 -0400

    formatting

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
   app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
